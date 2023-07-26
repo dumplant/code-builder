1fca1b55ed15212fc32679a799888161c7e44961
dumplant <2437789306@qq.com>
2023-07-26 19:18:09 +0800

    feat(custom): 表格弹窗配置

---
+++ src/packages/table-editor.jsx
@@ -0,0 +1,43 @@
+import deepcopy from 'deepcopy';
+import { ElButton, ElTag } from 'element-plus';
+import { defineComponent, computed } from 'vue';
+import { $tableDialog } from '@/components/TableDialog';
+export default defineComponent({
+  props: {
+    propConfig: { type: Object },
+    modelValue: { type: Array },
+  },
+  emits: ['updata:modelValue'],
+  setup(props, ctx) {
+    const data = computed({
+      get() {
+        return props.modelValue || [];
+      },
+      set(newValue) {
+        ctx.emit('update:modelValue', deepcopy(newValue));
+      },
+    });
+    const add = () => {
+      $tableDialog({
+        config: props.propConfig,
+        data: data.value,
+        onConfirm: (newValue) => {
+          data.value = newValue; //当点击确认时 更新数据
+        },
+      });
+    };
+    return () => {
+      return (
+        <div>
+          {!data.value ||
+            (data.value.length === 0 && (
+              <ElButton onClick={add}>添加</ElButton>
+            ))}
+          {(data.value || []).map((item) => (
+            <ElTag onClick={add}>{item[props.propConfig.table.label]}</ElTag>
+          ))}
+        </div>
+      );
+    };
+  },
+});
